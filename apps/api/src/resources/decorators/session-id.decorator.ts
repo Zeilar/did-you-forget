8f32ae6e338541c0a03f0d411b3697e823c0f8f7
PhilipAngelinNE <philip.angelin@ne.se>
2024-11-12 15:37:09 +0100

    docs: add comment

--- apps/api/src/resources/decorators/session-id.decorator.ts
+++ apps/api/src/resources/decorators/session-id.decorator.ts
@@ -2,6 +2,9 @@ import { createParamDecorator, UnauthorizedException, type ExecutionContext } fr
 import type { Request } from "express";
 import config from "../../config";
 
+/**
+ * Should only be used in routes where a session id is mandatory to continue.
+ */
 export const SessionId = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext): ParameterDecorator => {
     const req: Request | undefined = ctx?.switchToHttp().getRequest();
