0d8c375bff6746b509396b26565c22655062f10b
PhilipAngelinNE <philip.angelin@ne.se>
2024-11-13 01:22:05 +0100

    refactor: remove caching as it's redundant

--- apps/api/src/app/index.module.ts
+++ apps/api/src/app/index.module.ts
@@ -1,29 +1,18 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { CacheInterceptor, CacheModule } from "@nestjs/cache-manager";
 import { AuthController } from "./index.controller";
-import { AppService } from "./index.service";
 import { UserModule } from "../resources/user/index.module";
 import { AuthModule } from "../resources/auth/index.module";
 import { NotificationModule } from "../resources/notification/index.module";
 import config from "../config";
-import { APP_INTERCEPTOR } from "@nestjs/core";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
-    CacheModule.register({ isGlobal: true, ttl: 10 }),
     UserModule,
     AuthModule,
     NotificationModule,
   ],
   controllers: [AuthController],
-  providers: [
-    AppService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor,
-    },
-  ],
 })
 export class AppModule {}
