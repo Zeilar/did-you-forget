45e9275b6d89615c5a09545bd27953631ce81084
Zeilar <philip_angelin@hotmail.com>
2024-11-20 01:15:16 +0100

    feat: change title color

--- apps/ui/src/components/Title/index.tsx
+++ apps/ui/src/components/Title/index.tsx
@@ -1,5 +1,5 @@
 import { Heading, type HeadingProps } from "@chakra-ui/react";
 
 export function Title(props: HeadingProps) {
-  return <Heading color="accent.600" as="h2" size="3xl" mb={2} {...props} />;
+  return <Heading as="h2" size="3xl" mb={2} {...props} />;
 }
