cfe0656903c2a66f562e05c74fc170519e625406
Zeilar <philip_angelin@hotmail.com>
2024-11-26 02:23:32 +0100

    feat: add route progress bar

--- apps/ui/src/components/Providers/index.tsx
+++ apps/ui/src/components/Providers/index.tsx
@@ -4,6 +4,7 @@ import type { PropsWithChildren } from "react";
 import { ChakraProvider, GlobalStyle } from "@chakra-ui/react";
 import { theme } from "../../theme";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { AppProgressBar } from "next-nprogress-bar";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
@@ -14,6 +15,11 @@ export function Providers({ children }: PropsWithChildren) {
     <ChakraProvider theme={theme}>
       <GlobalStyle />
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <AppProgressBar
+        shallowRouting
+        options={{ showSpinner: false }}
+        color="var(--chakra-colors-primary-500)"
+      />
     </ChakraProvider>
   );
 }
