78a45ab1e61be1c9d8b2b7ca48908a45672b9bb2
Zeilar <philip_angelin@hotmail.com>
2024-11-26 02:19:27 +0100

    refactor: split components

---
+++ apps/ui/src/features/notification/components/Notification/components/Reminders/components/Reminder/components/EditDialog/index.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
+  PinInput,
+  PinInputField,
+  type UseDisclosureReturn,
+} from "@chakra-ui/react";
+import { Input } from "@ui/components";
+import { Controller, type FieldError, type Control } from "react-hook-form";
+import type { EditFields } from "../..";
+import type { BaseSyntheticEvent } from "react";
+
+interface EditDialogProps {
+  disclosure: UseDisclosureReturn;
+  isLoading: boolean;
+  onSubmit: (e?: BaseSyntheticEvent) => Promise<void>;
+  control: Control<EditFields>;
+  error: FieldError | undefined;
+}
+
+export function EditDialog({ disclosure, isLoading, onSubmit, control, error }: EditDialogProps) {
+  return (
+    <Modal isOpen={disclosure.isOpen} onClose={disclosure.onClose}>
+      <ModalOverlay />
+      <ModalContent w="fit-content" as="form" onSubmit={onSubmit}>
+        <ModalBody>
+          <Controller
+            control={control}
+            name="reminder"
+            render={({ field }) => (
+              <FormControl>
+                <Flex pt={4} gap={2}>
+                  <PinInput size="lg" placeholder="0" value={field.value} onChange={field.onChange}>
+                    <PinInputField order={1} as={Input} aria-label="Hours" placeholder="M" />
+                    <PinInputField order={2} as={Input} aria-label="Hours" placeholder="M" />
+                    <PinInputField order={4} as={Input} aria-label="Minutes" placeholder="M" />
+                    <PinInputField order={5} as={Input} aria-label="Minutes" placeholder="M" />
+                    <Box sx={{ all: "unset", order: 3 }}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="48"
+                        viewBox="0 0 16 48"
+                      >
+                        <Box as="circle" cx="8" cy="16" r="2" fill="border" />
+                        <Box as="circle" cx="8" cy="32" r="2" fill="border" />
+                      </svg>
+                    </Box>
+                  </PinInput>
+                </Flex>
+                {error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+              </FormControl>
+            )}
+          />
+        </ModalBody>
+        <ModalFooter gap={2}>
+          <Button isLoading={isLoading} type="submit">
+            Save
+          </Button>
+          <Button variant="ghost" onClick={disclosure.onClose}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
--- apps/ui/src/features/notification/components/Notification/components/Reminders/components/Reminder/index.tsx
+++ apps/ui/src/features/notification/components/Notification/components/Reminders/components/Reminder/index.tsx
@@ -1,34 +1,19 @@
 "use client";
 
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  IconButton,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalOverlay,
-  PinInput,
-  PinInputField,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Flex, IconButton, useDisclosure } from "@chakra-ui/react";
 import type { NotificationDto } from "@did-you-forget/dto";
-import { Input } from "@ui/components";
 import { useEditNotification } from "@ui/features/notification/hooks";
 import humanFormat from "human-format";
 import { useCallback } from "react";
-import { Controller, type SubmitHandler, useForm } from "react-hook-form";
+import { type SubmitHandler, useForm } from "react-hook-form";
 import { BsTrash } from "react-icons/bs";
+import { EditDialog } from "./components";
 
 interface ReminderProps extends Pick<NotificationDto, "id" | "reminders"> {
   reminder: string;
 }
 
-interface Fields {
+export interface EditFields {
   reminder: string;
 }
 
@@ -41,11 +26,11 @@ const timeScale = new humanFormat.Scale({
 });
 
 export function Reminder({ id, reminder, reminders }: ReminderProps) {
-  const { control, formState, handleSubmit } = useForm<Fields>();
-  const modal = useDisclosure();
-  const { mutate, isLoading } = useEditNotification(id, modal.onClose);
+  const editForm = useForm<EditFields>();
+  const editModal = useDisclosure();
+  const { mutate, isLoading } = useEditNotification(id, editModal.onClose);
 
-  const onSubmit = useCallback<SubmitHandler<Fields>>(
+  const onEditSubmit = useCallback<SubmitHandler<EditFields>>(
     (fields) => {
       const [h1, h2, m1, m2] = fields.reminder;
       const hours = parseInt(`${h1}${h2}`);
@@ -59,80 +44,39 @@ export function Reminder({ id, reminder, reminders }: ReminderProps) {
   );
 
   return (
-    <Flex align="center" gap={2}>
-      <Modal isOpen={modal.isOpen} onClose={modal.onClose}>
-        <ModalOverlay />
-        <ModalContent w="fit-content" as="form" onSubmit={handleSubmit(onSubmit)}>
-          <ModalBody>
-            <Controller
-              control={control}
-              name="reminder"
-              render={({ field }) => (
-                <FormControl>
-                  <Flex pt={4} gap={2}>
-                    <PinInput
-                      size="lg"
-                      placeholder="0"
-                      value={field.value}
-                      onChange={field.onChange}
-                    >
-                      <PinInputField order={1} as={Input} aria-label="Hours" placeholder="M" />
-                      <PinInputField order={2} as={Input} aria-label="Hours" placeholder="M" />
-                      <PinInputField order={4} as={Input} aria-label="Minutes" placeholder="M" />
-                      <PinInputField order={5} as={Input} aria-label="Minutes" placeholder="M" />
-                      <Box sx={{ all: "unset", order: 3 }}>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="48"
-                          viewBox="0 0 16 48"
-                        >
-                          <Box as="circle" cx="8" cy="16" r="2" fill="border" />
-                          <Box as="circle" cx="8" cy="32" r="2" fill="border" />
-                        </svg>
-                      </Box>
-                    </PinInput>
-                  </Flex>
-                  {formState.errors.reminder && (
-                    <FormErrorMessage>{formState.errors.reminder.message}</FormErrorMessage>
-                  )}
-                </FormControl>
-              )}
-            />
-          </ModalBody>
-          <ModalFooter gap={2}>
-            <Button isLoading={isLoading} type="submit">
-              Save
-            </Button>
-            <Button variant="ghost" onClick={modal.onClose}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-      <Flex
-        as="button"
-        w="full"
-        justifyContent="start"
-        p={3}
-        bgColor="bg.paper"
-        rounded="md"
-        opacity={!isLoading ? 1 : 0.5}
-        disabled={isLoading}
-        onClick={modal.onOpen}
-      >
-        {humanFormat(parseInt(reminder), { scale: timeScale })}
+    <>
+      <Flex align="center" gap={2}>
+        <Flex
+          as="button"
+          w="full"
+          justifyContent="start"
+          p={3}
+          bgColor="bg.paper"
+          rounded="md"
+          opacity={!isLoading ? 1 : 0.5}
+          disabled={isLoading}
+          onClick={editModal.onOpen}
+        >
+          {humanFormat(parseInt(reminder), { scale: timeScale })}
+        </Flex>
+        <IconButton
+          variant="outline"
+          borderColor="text.danger"
+          color="text.danger"
+          icon={<BsTrash />}
+          aria-label="Delete"
+          isLoading={isLoading}
+          size="lg"
+          onClick={() => mutate({ reminders: reminders.filter((element) => element !== reminder) })}
+        />
       </Flex>
-      <IconButton
-        variant="outline"
-        borderColor="text.danger"
-        color="text.danger"
-        icon={<BsTrash />}
-        aria-label="Delete"
+      <EditDialog
+        control={editForm.control}
+        disclosure={editModal}
+        error={editForm.formState.errors.reminder}
         isLoading={isLoading}
-        size="lg"
-        onClick={() => mutate({ reminders: reminders.filter((element) => element !== reminder) })}
+        onSubmit={editForm.handleSubmit(onEditSubmit)}
       />
-    </Flex>
+    </>
   );
 }
