e4f45e44dcd48ed716bc2d28a5abd0501c47ed4c
Zeilar <philip_angelin@hotmail.com>
2024-12-09 06:32:24 +0100

    feat: error handling

--- apps/ui/src/app/(protected)/account/Logout.tsx
+++ apps/ui/src/app/(protected)/account/Logout.tsx
@@ -2,14 +2,24 @@
 
 import { Button } from "@chakra-ui/react";
 import { clientFetch } from "@ui/common/fetchers/client";
+import { useToast } from "@ui/hooks";
 import { LogOut } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useMutation } from "react-query";
 
 export function Logout() {
+  const toast = useToast();
   const { push } = useRouter();
   const { isLoading, mutate } = useMutation("logout", () => clientFetch("/auth/logout", "POST"), {
     onSuccess: () => push("/login"),
+    onError: (error) => {
+      console.error(error);
+      toast({
+        title: "Error",
+        description: "An unexpected error occurred.",
+        status: "error",
+      });
+    },
   });
 
   return (
